f816fde68b9c637126c8dab4090be400007b93e7
hemantkumarsuman <hemantkumar1699@gmail.com>
2025-12-21 11:52:01 +0530

    exploring GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req,res)=>{
-//     res.send("Hello from server");
-// })
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Hemant", lastName: "Kumar"})
+});
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to DB");;
+    res.send("Data saved Successfully");    
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted Successfully");    
+})
 
 app.use("/test",(req,res)=>{
     res.send("Hello from server using nodemon");
