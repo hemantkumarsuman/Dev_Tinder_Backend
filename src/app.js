52cf25e0f74537e584cd1571e8febc5dab822ef6
hemantkumarsuman <hemantkumar1699@gmail.com>
2025-12-27 12:14:40 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,22 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstName: "Hemant", lastName: "Kumar"})
-});
-
-app.post("/user",(req,res)=>{
-    console.log("Save data to DB");;
-    res.send("Data saved Successfully");    
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Data deleted Successfully");    
-})
-
-app.use("/test",(req,res)=>{
-    res.send("Hello from server using nodemon");
-})
+app.use("/user",(req,res,next)=>{
+    console.log("User Route");
+    next();    
+},
+(req, res,next)=>{
+    console.log("2nd route");
+    //res.send("2nd response");
+    next()
+},
+(req,res,next)=>{
+    console.log("3rd route");
+    //res.send("3rd response");
+    next()
+},
+(req,res,next)=>{
+    console.log("4th route");
+    // res.send("4th response")
+    //next()
+}
+)
 
 app.listen(3000, ()=>{
     console.log("Server started successfully");
